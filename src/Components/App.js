e256a902eb84fa304e17bd345680523d3b5debb2
Alexandra Campo <alexandracampo86@gmail.com>
2023-05-25 23:07:07 +0200

    create project

---
+++ src/Components/App.js
@@ -0,0 +1,22 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <>
+
+      <h1>To-do List 📋</h1>
+
+      <form>
+        <input type='text' placeholder='Añade aquí tu tarea'></input>
+        <button>Agregar tarea nueva</button>
+      </form>
+
+      <div>
+
+      </div>
+
+    </>
+  );
+}
+
+export default App;
