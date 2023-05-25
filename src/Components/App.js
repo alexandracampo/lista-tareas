bad25583cd68477aaeb750b5800ed05ef4bf9a3b
Alexandra Campo <alexandracampo86@gmail.com>
2023-05-26 01:53:03 +0200

    add input handle function of new task

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,24 +1,33 @@
 import '../styles/App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleChangeInputTask = (ev) => {
+    setNewTask(ev.target.value);
+  }
+
+  const handleNewTask = (ev) => {
+    setTasks(newTask);
+  }
+
   return (
     <>
 
       <h1>To-do List 📋</h1>
 
       <form>
-        <input type='text' placeholder='Añade aquí tu tarea'></input>
-        <button>Agregar tarea nueva</button>
+        <input onChange={handleChangeInputTask} type='text' placeholder='Añade aquí tu tarea'></input>
+        <button onClick={handleNewTask}>Agregar tarea nueva</button>
       </form>
 
       <ul>
-        <li>
-          <p></p>
-          <button>editar</button>
-          <button>checked</button>
-          <button>borrar</button>
-        </li>
+
       </ul>
+
     </>
   );
 }
