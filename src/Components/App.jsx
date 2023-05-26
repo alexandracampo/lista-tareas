a5ae9660763325a31af39c470840c99315a03ea7
Alexandra Campo <alexandracampo86@gmail.com>
2023-05-26 12:17:23 +0200

    add new feature: render task list

---
+++ src/Components/App.jsx
@@ -0,0 +1,40 @@
+import '../styles/App.css';
+import React, { useState } from 'react';
+/* import ListTasks from './ListTasks';
+ */
+function App() {
+
+  const [inputTask, setInputTask] = useState('');
+  const [tasksList, setTasksList] = useState([]);
+
+  const handleInputTask = (ev) => {
+    setInputTask(ev.target.value);
+  }
+
+  const handleBtnNewTask = (ev) => {
+    ev.preventDefault();
+    if (inputTask.trim() !== '') {
+      setTasksList([...tasksList, inputTask]);
+      setInputTask('');
+    }
+  }
+
+  return (
+    <>
+      <h1>To-do List 📋</h1>
+
+      <form>
+        <input onChange={handleInputTask} type='text' placeholder='Añade aquí tu tarea'></input>
+        <button onClick={handleBtnNewTask}>Agregar tarea nueva</button>
+      </form>
+
+      <ul>
+        {tasksList.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/ListTasks.jsx
@@ -0,0 +1,15 @@
+/* import { useState } from 'react';
+
+
+function ListTasks() {
+
+
+
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default ListTasks; */
