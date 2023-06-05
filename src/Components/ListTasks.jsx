6a3a92d27f07abac0cc77d3891dfea226dc1f9e7
Alexandra Campo <alexandracampo86@gmail.com>
2023-06-05 17:50:17 +0200

    add new component

--- src/Components/ListTasks.jsx
+++ src/Components/ListTasks.jsx
@@ -1,40 +1,17 @@
 import '../styles/App.scss';
 import '../styles/ListTasks.scss';
 import React, { useState } from 'react';
-import { FcFullTrash } from "react-icons/fc";
+import Task from './Task';
 
 function ListTasks({ tasksList }) {
-
-  const [checkedTasks, setCheckedTasks] = useState({});
-  console.log(checkedTasks)
-
-  const handleCheckedTask = (ev) => {
-    setCheckedTasks(prevList => ({
-      ...prevList,
-      [ev.target.id]: ev.target.checked
-    }));
-  }
 
   return (
     <div className='list-section'>
       <ul className='list-container'>
 
-        {tasksList.map((task, index) => (
-          <li className='task' key={index}>
-            <input
-              id={index}
-              onChange={handleCheckedTask}
-              type="checkbox"
-              value={checkedTasks[index]}
-              className={`${checkedTasks[index] ? 'task__checked' : 'task__checkbox'}`}
-            >
-            </input>
-            <p className='task__text'>{task}</p>
-            <button className='task__remove'>
-              <FcFullTrash className='img-trash' />
-            </button>
-          </li>
-        ))}
+        <Task
+          tasksList={tasksList}
+        />
 
       </ul>
     </div>
---
+++ src/Components/Task.jsx
@@ -0,0 +1,40 @@
+import '../styles/App.scss';
+import '../styles/Task.scss';
+import React, { useState } from 'react';
+import { FcFullTrash } from "react-icons/fc";
+
+function Task({ tasksList }) {
+
+    const [checkedTasks, setCheckedTasks] = useState({});
+    console.log(checkedTasks)
+
+    const handleCheckedTask = (ev) => {
+        setCheckedTasks(prevList => ({
+            ...prevList,
+            [ev.target.id]: ev.target.checked
+        }));
+    }
+
+    return (
+        <>
+            {tasksList.map((task, index) => (
+                <li className='task' key={index}>
+                    <input
+                        id={index}
+                        onChange={handleCheckedTask}
+                        type="checkbox"
+                        value={checkedTasks[index]}
+                        className={`${checkedTasks[index] ? 'task__checked' : 'task__checkbox'}`}
+                    >
+                    </input>
+                    <p className='task__text'>{task}</p>
+                    <button className='task__remove'>
+                        <FcFullTrash className='img-trash' />
+                    </button>
+                </li>
+            ))}
+        </>
+    );
+}
+
+export default Task;
