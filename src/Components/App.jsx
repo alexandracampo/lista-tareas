2dc0b383af8c789c7a6b7b0337fb6516b9bd4627
Alexandra Campo <alexandracampo86@gmail.com>
2023-07-04 09:17:31 +0200

    add localStorage

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,10 +3,19 @@ import React, { useState } from 'react';
 import Header from './Header';
 import ListTasks from './ListTasks';
 import Form from './Form';
+import ls from '../services/localStorage';
 
 function App() {
 
-  const [tasksList, setTasksList] = useState([]);
+  const [tasksList, setTasksList] = useState(ls.get('persistent_tasks_list'));
+
+  //Usamos useEffect para guardar los datos en el local storage
+  // Cuando React renderice y pinte el HTML en la página ejecutará este useEffect
+  // Este useEffect solo se ejecutará cuando cambie el listado
+  useEffect(() => {
+    // Guardamos  en el local storage
+    ls.set('persistent_tasks_list', tasksList);
+  }, [tasksList]);
 
   return (
     <>
